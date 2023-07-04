480e2772e82054d619e088221c52d6fe04799096
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-04 23:03:46 +0530

    Create overriding1.java

---
+++ overriding1.java
@@ -0,0 +1,22 @@
+/*method overriding*/
+class shape
+{
+    void draw()
+    {
+        System.out.println("Can't Say shape Type");
+    }
+}
+class square extends shape
+{
+    void draw2() /* output will be "Can't Say shape Type" */
+    {
+        System.out.println("square shape")
+    }
+}
+class demo
+{
+    public static void main(String[] args) {
+        shape r=new square();
+        r.draw();
+    }
+}
